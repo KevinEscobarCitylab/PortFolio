b1850c6230a6090100806205d4912cfa3362c63e
Kevin Alexander Escobar Aguilar <108683485+KevinEscobarCitylab@users.noreply.github.com>
2023-12-18 11:54:01 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["SQL Developer", "Backend development Jr.", "Web designing Jr.", "NoSQL Developer Jr,", "DBA Jr."],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
